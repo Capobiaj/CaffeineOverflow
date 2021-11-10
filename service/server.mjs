c457dc1b6f0e361b50072d49d4978ac4a1513a11
Joseph Capobianco <capobiaj@oregonstate.edu>
2021-11-10 16:44:50 +0800

    finished wikitravel scraper service

--- service/server.mjs
+++ service/server.mjs
@@ -18,27 +18,88 @@ app.get('/wikiscrape/:name', async function(req, res) {
         let data = {}
         let relevantPages = []
         let exactPage = []
-        let pageText = []
+        let pageId = 0
         let keys = response1data.query.search
         for (let x in keys) {
             if (keys[x].title === page) {
-                exactPage.push(keys[x])
+                exactPage.push(keys[x].title)
+                pageId += keys[x].pageid
             }else {
                 relevantPages.push(keys[x].title)
             }
         }
-        data['pagetitle'] = exactPage
-        data['totalhits'] = response1data.query.searchinfo.totalhits
-        data['relevantpages'] = relevantPages
+        // validates that there is an exact match for the page
         if (exactPage.length !== 0) {
-            const responseHTML = await fetch(`https://wikitravel.org/wiki/en/api.php?action=parse&page=${page}&prop=text&format=json`)
-            const responseHTMLdata = await responseHTML.json()
-            let pageHTML = responseHTMLdata.parse.text['*']
-            pageText.push(htmlToText(pageHTML))
-            data['html'] = pageHTML
-            data['text'] = pageText
+
+            // grabs summary section (section 0) as HTML and plain text. HTML is in summaryHTML and plain text is in summaryText
+            let summaryText = []
+            const responseHTML = await fetch(`https://wikitravel.org/wiki/en/api.php?action=parse&page=${page}&prop=wikitext&section=0&format=json`)
+            const responseHTMLData = await responseHTML.json()
+            let summaryHTML = responseHTMLData.parse.wikitext['*']
+            summaryText.push(htmlToText(summaryHTML))
+            
+            // grabs images
+            let images = []
+            const responseImages = await fetch(`https://wikitravel.org/wiki/en/api.php?action=parse&page=${page}&prop=images&format=json`)
+            const responseImagesData = await responseImages.json()
+            for (let w in responseImagesData.parse.images) {
+                images.push(responseImagesData.parse.images[w])
+            }
+            
+
+            // grabs external links puts them in extLinks variable
+            let extLinks = []
+            const responseExtLinks = await fetch(`https://wikitravel.org/wiki/en/api.php?action=parse&page=${page}&prop=externallinks&format=json`)
+            const responseExtLinksData = await responseExtLinks.json()
+            for (let v in responseExtLinksData.parse.externallinks) {
+                extLinks.push(responseExtLinksData.parse.externallinks[v])
+            }
+
+            // grabs internal links and puts them in intLinks variable
+            let intLinks = []
+            const responseIntLinks = await fetch(`https://wikitravel.org/wiki/en/api.php?action=parse&page=${page}&prop=links&format=json`)
+            const responseIntLinksData = await responseIntLinks.json()
+            for (let z in responseIntLinksData.parse.links) {
+                intLinks.push(responseIntLinksData.parse.links[z]["*"])
+            }
+
+            // grabs sections to find the index for the understand section. Places the index number in understand variable
+            let understand = 0
+            const responseSections = await fetch(`https://wikitravel.org/wiki/en/api.php?action=parse&page=${page}&prop=sections&format=json`)
+            const responseSectionsData = await responseSections.json()
+            for (let y in responseSectionsData.parse.sections) {
+                if (responseSectionsData.parse.sections[y].line === "Understand") {
+                    understand += responseSectionsData.parse.sections[y].index
+                }
+            }
+            
+            // grabs Understand section as HTML and plain text based on section index from understand variable. HTML is in understandHTML and plain text is in understandText
+            let understandText = []
+            const responseUnderstand = await fetch(`https://wikitravel.org/wiki/en/api.php?action=parse&page=${page}&prop=wikitext&section=${understand}&format=json`)
+            const responseUnderstandData = await responseUnderstand.json()
+            let understandHTML = responseUnderstandData.parse.wikitext['*']
+            understandText.push(htmlToText(understandHTML))
+            
+            
+            // creates response json by adding key value pairs to data object
+            data['pagetitle'] = exactPage
+            data['relevantpages'] = relevantPages
+            data['images'] = images
+            data['externallinks'] = extLinks
+            data['internallinks'] = intLinks
+            data['summaryhtml'] = summaryHTML
+            data['summarytext'] = summaryText
+            data['understandhtml'] = understandHTML
+            data['understandtext'] = understandText
+            
+            // sends data object as the json response
             res.send(data)
 
+        } else {
+            data['page'] = 'Unable to find exact page - consider inputting one of the relevant pages instead'
+            data['totalhits'] = response1data.query.searchinfo.totalhits
+            data['relevantpages'] = relevantPages
+            res.send(data)
         }
     } catch (error) {
         console.log(error);
